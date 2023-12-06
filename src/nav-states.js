88ddb0f7991b5c5684fc9a70ea7e88f5d8d19198
Austin Thesing <athesing@point.com>
2023-12-06 16:00:03 -0700

    Update nav-states.js

--- src/nav-states.js
+++ src/nav-states.js
@@ -1,123 +1,104 @@
+// Event listener for when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", (event) => {
-  let navbarAttribute = document.body.getAttribute("navbar-default");
-  let navbarComponent = document.querySelector(".navbar_component_updated");
-  let logoColorElement = document.querySelector(".logo-color");
-  let logoWhiteElement = document.querySelector(".logo-white");
-  let navDropdownTrigger = document.querySelectorAll(".nav_dropdown_trigger");
-  let navbarSingleLink = document.querySelectorAll(".navbar_single_link");
-  let navbarBtnPrimary = document.querySelector(".navbar_btn[navbar-btn='primary']");
-  let navbarBtnSecondary = document.querySelector(".navbar_btn[navbar-btn='secondary']");
-  let menuIconLines = document.querySelectorAll(".menu-icon1_line-top, .menu-icon1_line-middle, .menu-icon1_line-bottom");
+  // Define all the elements that will be manipulated
+  // These are mostly elements related to the navigation bar and its components
+  const elements = {
+    navbarAttribute: document.body.getAttribute("navbar-default"), // Attribute of the navbar
+    navbarComponent: document.querySelector(".navbar_component_updated"), // The navbar component
+    logoColorElement: document.querySelector(".logo-color"), // The colored logo
+    logoWhiteElement: document.querySelector(".logo-white"), // The white logo
+    navDropdownTrigger: document.querySelectorAll(".nav_dropdown_trigger"), // The dropdown triggers in the navbar
+    navbarSingleLink: document.querySelectorAll(".navbar_single_link"), // The single links in the navbar
+    navbarBtnPrimary: document.querySelector(".navbar_btn[navbar-btn='primary']"), // The primary button in the navbar
+    navbarBtnSecondary: document.querySelector(".navbar_btn[navbar-btn='secondary']"), // The secondary button in the navbar
+    menuIconLines: document.querySelectorAll(".menu-icon1_line-top, .menu-icon1_line-middle, .menu-icon1_line-bottom"), // The lines in the menu icon
+  };
 
-  if (navbarAttribute === "filled") {
-    navbarComponent.style.backgroundColor = "#FFFFFF";
-    logoColorElement.style.opacity = "1";
-    navDropdownTrigger.forEach((element) => {
-      element.style.color = "#444";
-      element.addEventListener("mouseover", function () {
-        element.style.color = "#016789";
-      });
-      element.addEventListener("mouseout", function () {
-        if (window.scrollY <= 120) {
-          element.style.color = "#FFFFFF";
-        } else {
-          element.style.color = "#444";
-        }
-      });
+  // Function to set styles for an element
+  // This function takes an element and a styles object as parameters
+  // It then applies each style in the styles object to the element
+  const setElementStyle = (element, styles) => {
+    for (let style in styles) {
+      element.style[style] = styles[style];
+    }
+  };
+
+  // Function to add a hover effect to an element
+  // This function takes an element and a color as parameters
+  // It then adds event listeners for mouseover and mouseout events
+  // On mouseover, the color of the element is changed to the provided color
+  // On mouseout, the color of the element is changed based on the scroll position
+  const addHoverEffect = (element, color) => {
+    element.addEventListener("mouseover", function () {
+      element.style.color = color;
     });
-    navbarSingleLink.forEach((element) => {
+    element.addEventListener("mouseout", function () {
+      element.style.color = window.scrollY <= 120 ? "#FFFFFF" : "#444";
+    });
+  };
+
+  // Check the navbar attribute
+  // If it is 'filled', apply certain styles to the elements
+  // If it is not 'filled', add a scroll event listener to apply styles based on the scroll position
+  if (elements.navbarAttribute === "filled") {
+    // Apply styles for a filled navbar
+    setElementStyle(elements.navbarComponent, { backgroundColor: "#FFFFFF" });
+    setElementStyle(elements.logoColorElement, { opacity: "1" });
+    elements.navDropdownTrigger.forEach((element) => {
+      setElementStyle(element, { color: "#444" });
+      addHoverEffect(element, "#016789");
+    });
+    elements.navbarSingleLink.forEach((element) => {
       if (!element.classList.contains("navbar_btn")) {
-        element.style.color = "#444";
-        element.addEventListener("mouseover", function () {
-          element.style.color = "#016789";
-        });
-        element.addEventListener("mouseout", function () {
-          if (window.scrollY <= 120) {
-            element.style.color = "#FFFFFF";
-          } else {
-            element.style.color = "#444";
-          }
-        });
+        setElementStyle(element, { color: "#444" });
+        addHoverEffect(element, "#016789");
       }
     });
-    navbarBtnPrimary.style.backgroundColor = "#F1B937";
-    navbarBtnPrimary.style.color = "#444";
-    navbarBtnSecondary.style.backgroundColor = "#F6F7F9";
-    navbarBtnSecondary.style.color = "#444";
-    menuIconLines.forEach((element) => {
-      element.style.backgroundColor = "#444";
+    setElementStyle(elements.navbarBtnPrimary, { backgroundColor: "#F1B937", color: "#444" });
+    setElementStyle(elements.navbarBtnSecondary, { backgroundColor: "#F6F7F9", color: "#444" });
+    elements.menuIconLines.forEach((element) => {
+      setElementStyle(element, { backgroundColor: "#444" });
     });
   } else {
+    // Add a scroll event listener to apply styles based on the scroll position
     window.addEventListener("scroll", function () {
-      if (window.scrollY > 120 && navbarComponent.style.backgroundColor === "transparent") {
-        navbarComponent.style.transition = "background-color 0.3s ease-in-out";
-        navbarComponent.style.backgroundColor = "#FFFFFF";
-        logoColorElement.style.transition = "opacity 0.3s ease-in-out";
-        logoColorElement.style.opacity = "1";
-        navDropdownTrigger.forEach((element) => {
-          element.style.color = "#444";
-          element.addEventListener("mouseover", function () {
-            element.style.color = "#016789";
-          });
-          element.addEventListener("mouseout", function () {
-            if (window.scrollY <= 120) {
-              element.style.color = "#FFFFFF";
-            } else {
-              element.style.color = "#444";
-            }
-          });
+      if (window.scrollY > 120 && elements.navbarComponent.style.backgroundColor === "transparent") {
+        // Apply styles for a scrolled down state
+        setElementStyle(elements.navbarComponent, { transition: "background-color 0.3s ease-in-out", backgroundColor: "#FFFFFF" });
+        setElementStyle(elements.logoColorElement, { transition: "opacity 0.3s ease-in-out", opacity: "1" });
+        elements.navDropdownTrigger.forEach((element) => {
+          setElementStyle(element, { color: "#444" });
+          addHoverEffect(element, "#016789");
         });
-        navbarSingleLink.forEach((element) => {
+        elements.navbarSingleLink.forEach((element) => {
           if (!element.classList.contains("navbar_btn")) {
-            element.style.color = "#444";
-            element.addEventListener("mouseover", function () {
-              element.style.color = "#016789";
-            });
-            element.addEventListener("mouseout", function () {
-              if (window.scrollY <= 120) {
-                element.style.color = "#FFFFFF";
-              } else {
-                element.style.color = "#444";
-              }
-            });
+            setElementStyle(element, { color: "#444" });
+            addHoverEffect(element, "#016789");
           }
         });
-        navbarBtnPrimary.style.backgroundColor = "#F4C65E";
-        navbarBtnPrimary.style.color = "#444";
-        navbarBtnSecondary.style.backgroundColor = "#F6F7F9";
-        navbarBtnSecondary.style.color = "#444";
-        menuIconLines.forEach((element) => {
-          element.style.backgroundColor = "#444";
+        setElementStyle(elements.navbarBtnPrimary, { backgroundColor: "#F4C65E", color: "#444" });
+        setElementStyle(elements.navbarBtnSecondary, { backgroundColor: "#F6F7F9", color: "#444" });
+        elements.menuIconLines.forEach((element) => {
+          setElementStyle(element, { backgroundColor: "#444" });
         });
       } else if (window.scrollY <= 120) {
-        navbarComponent.style.backgroundColor = "transparent";
-        logoColorElement.style.opacity = "0";
-        navDropdownTrigger.forEach((element) => {
-          element.style.color = "#FFFFFF";
-          element.addEventListener("mouseover", function () {
-            element.style.color = "#016789";
-          });
-          element.addEventListener("mouseout", function () {
-            element.style.color = "#FFFFFF";
-          });
+        // Apply styles for a scrolled up state
+        setElementStyle(elements.navbarComponent, { backgroundColor: "transparent" });
+        setElementStyle(elements.logoColorElement, { opacity: "0" });
+        elements.navDropdownTrigger.forEach((element) => {
+          setElementStyle(element, { color: "#FFFFFF" });
+          addHoverEffect(element, "#016789");
         });
-        navbarSingleLink.forEach((element) => {
+        elements.navbarSingleLink.forEach((element) => {
           if (!element.classList.contains("navbar_btn")) {
-            element.style.color = "#FFFFFF";
-            element.addEventListener("mouseover", function () {
-              element.style.color = "#016789";
-            });
-            element.addEventListener("mouseout", function () {
-              element.style.color = "#FFFFFF";
-            });
+            setElementStyle(element, { color: "#FFFFFF" });
+            addHoverEffect(element, "#016789");
           }
         });
-        navbarBtnPrimary.style.backgroundColor = "";
-        navbarBtnPrimary.style.color = "";
-        navbarBtnSecondary.style.backgroundColor = "";
-        navbarBtnSecondary.style.color = "";
-        menuIconLines.forEach((element) => {
-          element.style.backgroundColor = "";
+        setElementStyle(elements.navbarBtnPrimary, { backgroundColor: "", color: "" });
+        setElementStyle(elements.navbarBtnSecondary, { backgroundColor: "", color: "" });
+        elements.menuIconLines.forEach((element) => {
+          setElementStyle(element, { backgroundColor: "" });
         });
       }
     });
