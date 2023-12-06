4795997804fc3f928d8d1cd7d9786b12de502e72
Austin Thesing <athesing@point.com>
2023-12-06 14:43:38 -0700

    Fixed scroll triggers for text color

--- src/nav-states.js
+++ src/nav-states.js
@@ -2,10 +2,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
   let navbarAttribute = document.body.getAttribute("navbar-default");
   let navbarComponent = document.querySelector(".navbar_component_updated");
   let logoColorElement = document.querySelector(".logo-color");
+  let navDropdownTrigger = document.querySelectorAll(".nav_dropdown_trigger");
+  let navbarSingleLink = document.querySelectorAll(".navbar_single_link");
 
   if (navbarAttribute === "filled") {
     navbarComponent.style.backgroundColor = "#FFFFFF";
     logoColorElement.style.opacity = "1";
+    navDropdownTrigger.forEach(element => {
+      element.style.color = "#444";
+    });
+    navbarSingleLink.forEach(element => {
+      element.style.color = "#444";
+    });
   } else {
     window.addEventListener("scroll", function () {
       if (window.scrollY > 120 && navbarComponent.style.backgroundColor === "transparent") {
@@ -13,9 +21,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
         navbarComponent.style.backgroundColor = "#FFFFFF";
         logoColorElement.style.transition = "opacity 0.5s ease-in-out";
         logoColorElement.style.opacity = "1";
+        navDropdownTrigger.forEach(element => {
+          element.style.color = "#444";
+        });
+        navbarSingleLink.forEach(element => {
+          element.style.color = "#444";
+        });
       } else if (window.scrollY <= 120) {
         navbarComponent.style.backgroundColor = "transparent";
         logoColorElement.style.opacity = "0";
+        navDropdownTrigger.forEach(element => {
+          element.style.color = "";
+        });
+        navbarSingleLink.forEach(element => {
+          element.style.color = "";
+        });
       }
     });
   }
